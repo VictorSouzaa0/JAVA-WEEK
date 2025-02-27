6d6c55bc4b66d882b40e40eac2e62e8fdf966a75
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-02-27 00:00:29 -0300

    first day

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//Package : folder name
+
+//class
+//public : Está classe pode ser acessada por outros pacotes
+public class Main {
+    //method                 Parametros da classe
+    public static void main(String[] args) {
+        //statements
+        System.out.printf("Hello and welcome!");
+
+    }
+}
