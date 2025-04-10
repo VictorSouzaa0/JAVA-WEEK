6101ea10d6915b4a7a064458b6ca30403ee1fa77
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-04-10 12:24:01 -0300

    array tips

--- src/arr/arr.java
+++ src/arr/arr.java
@@ -6,16 +6,29 @@ import java.util.Scanner;
 public class arr {
 
     public static void main(String[] args) {
-        Scanner input =  new Scanner(System.in);
-        ArrayList<Integer>values = new ArrayList<>();
 
-        boolean into = true;
+        //Array Unidimensional
+        //Se trata de um array que tem um tamanho fixo e não cresce dinamicamente
 
-        while (into = true){
-
-            System.out.println("Insira um valor");
-            double value1 = input.nextDouble();
+        String[]cars = {"Audi","Volkswagen","Toyota","GM"};
+        cars[0] = "Nissan";
+        for(String Car : cars){
+            System.out.println(" - " + Car);
         }
+        //Arrayy Bidmensional
 
+        String [][]Cars = {
+                {"M3","Cayenne","G63"},//indice 0
+                {"Corolla","Nivus","Subaru"}, // Indice 1
+                {"Compass","Commander","Civic"}// indice 2
+        };
+        //Priemiro for para listar cada proprietário de acordo com o índice, começando com 1
+        for(int x = 0; x < Cars.length; x++){
+            System.out.println("Proprietário " + (x+1) + " :");
+            // para cada proprietário que ele passar ele vai listar cada índice
+            for(int j = 0; j < Cars[x].length; j++){
+                System.out.println(" - " +Cars[x][j] + " ");
+            }
+        }
     }
 }
