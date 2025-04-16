5cfdab1e87a0f76b15c25ddb03ad0c517e0e560b
VictorSouzaa0 <victor.r.souza05@gmail.com>
2025-04-16 08:05:07 -0300

    math

---
+++ src/Mathh/Maath.java
@@ -0,0 +1,21 @@
+package Mathh;
+public class Maath {
+    public static void main(String[] args) {
+
+
+        //Math.max : Retorna o maior o valor entre os valores (x,y)
+        System.out.println(Math.max(10,4));
+
+        //Math.min : Retorna o menor valor entre os valores (x,y)
+        System.out.println(Math.min(90,89));
+
+        //Math.sqrt : retorna o número dele elevado ao quadrado
+        System.out.println(Math.sqrt(81));
+
+        //Math.abs : retorna o número absoluto (positivo) do velor de x:
+        System.out.println(Math.abs(-9));
+
+        //Math.random : retorna o número  aletatório entre 0.0 e 1.0
+        System.out.println(Math.random());
+    }
+}
